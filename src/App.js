f368fb2ff1867259623d03522132b3c73469d77e
Emily <eleblanc@tvo.org>
2022-09-14 11:30:49 -0400

    added input style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react'
 import './App.css';
+import ListItem from './ListItem';
 
 function App() {
   return (
     <div className="list">
+      <ListItem/>
+      <ListItem/>
+      <ListItem/>
     </div>
   );
 }
---
+++ src/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ListItem(){
+  return (
+  <div className='list-item'>
+    <input type="checkbox"/>
+    <p>walk the dog</p>
+  </div>
+  )
+}
+
+export default ListItem; 
